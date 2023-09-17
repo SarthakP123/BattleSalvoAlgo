bb9191c203d6324d52c97ca7dd1f608e70c87a2f
Sarthak Pokharel <84484753+SarthakP123@users.noreply.github.com>
2023-09-17 18:38:44 -0400

    Delete pa04-roadkillteam-algorithm directory

--- pa04-roadkillteam-algorithm/src/main/java/cs3500/pa04/model/GameType.java
+++
@@ -1,9 +0,0 @@
-package cs3500.pa04.model;
-
-/**
- * Represents the two different game types
- */
-public enum GameType {
-  SINGLE,
-  MULTI
-}
--- pa04-roadkillteam-algorithm/src/main/java/cs3500/pa04/model/ShipDirection.java
+++
@@ -1,9 +0,0 @@
-package cs3500.pa04.model;
-
-/**
- * Represents the direction the ship is facing
- */
-public enum ShipDirection {
-  HORIZONTAL,
-  VERTICAL
-}
